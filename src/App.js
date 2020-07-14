13f4bc4bc8ba8cb2f82ed9553e355503149d0a95
kevinneyer <kevinneyer@gmail.com>
2020-07-14 11:18:34 -0400

    first commit - 1.5 deliverable

--- src/App.js
+++ src/App.js
@@ -7,12 +7,23 @@ let API_ENDPOINT = `http://localhost:6001/songs`
 class App extends React.Component {
   state = {
     /* TODO: What should go in state here?? Anything we don't want to have to fetch again for instance...? */
+      SongList: []
   }
   
+  fetchSongs = () => {
+    fetch(API_ENDPOINT)
+    .then(response => response.json())
+    .then(data => {
+        this.setState({
+          SongList: data
+        })
+    })
+  } 
+
   renderNav = () => {
     return (
       <div className="simple-flex-row">
-        <button onClick={null /* TODO: Put your method to fetch the songs */}>Get Songs</button> 
+        <button onClick={this.fetchSongs}>Get Songs</button> 
         <h1>S-not-ify 🐽</h1>
         <input placeholder="Search by title or artist..."/>
       </div>
@@ -20,13 +31,14 @@ class App extends React.Component {
   }
 
   render(){
+    console.log(this.state)
     return (
       <div className="App">
         {this.renderNav()} {/** The renderNav method renders a div holding the button to get songs and the title */}
-        <MainContainer /> {/** TODO: What props do I need? */}
+        <MainContainer SongList={this.state.SongList}/> {/** TODO: What props do I need? */}
       </div>
     );
   }
 }
 
-export default App;
+export default App
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -2,7 +2,9 @@ import React from 'react';
 import SongList from './SongList';
 import Queue from './Queue';
 
+
 const MainContainer = props => {
+    console.log(props)
     return (
         <div className="simple-flex-row top">
             <SongList /> {/** TODO: What props do I need? */}
