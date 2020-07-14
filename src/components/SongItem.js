4dafa8b6c841a05a95c77b1e8c5c07b1a18eacb7
github username <github email address>
2020-07-14 12:40:01 -0400

    got favorite DOM manipulation

--- src/components/SongItem.js
+++ src/components/SongItem.js
@@ -1,16 +1,44 @@
 import React from 'react';
 
-const SongItem = props => {
+class SongItem extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            likes: 0,
+            favorite: false
+        }
+    }
+    
+    
+
+    handleClick = () => {
+        this.setState(previousState => {
+           return{
+            likes: previousState.likes + 1
+           } 
+        })
+    }
+
+    heartFill = () => {
+        this.setState(previousState => {
+            return {
+                favorite: !previousState.favorite
+            }
+        })
+    }
+
+    render() {
     return (
         <tr>
-            <td>{props.title}</td>
-            <td>{props.artist}</td>
+            <td>{this.props.title}</td>
+            <td>{this.props.artist}</td>
             <td><button onClick={null /* Put your click handler here */}>Play Now</button></td>
             <td><button onClick={null /* Put your click handler here */}>Add to Queue</button></td>
-            <td onClick={null /* Put your click handler here */}>{props.favorite ? "💚" : "♡"}</td>
-            <td>Likes: 0</td>
+            <td onClick={this.heartFill}>{this.state.favorite ? "💚" : "♡"}</td>
+            <td onClick={this.handleClick}>Likes: {this.state.likes}</td>
         </tr>
     )
+}
 }
 
 export default SongItem;
